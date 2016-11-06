d78350212b302174e608afc0ac01b94ad9597db2
bhaveshAn <bhaveshanand7@gmail.com>
2016-11-05 23:28:56 -0700

    Created Screens

---
+++ main.py
@@ -0,0 +1,34 @@
+import kivy
+kivy.require('1.8.0')
+from kivy.app import App 
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.label import Label 
+from kivy.uix.screenmanager import ScreenManager,Screen,FadeTransition
+
+
+class FirstScreen(Screen):
+	#l=Label(text="Hello Allo")
+
+
+	pass
+
+class CountryOption(Screen):
+
+	pass
+
+class VerificationCode(Screen):
+	pass
+
+class ScreenManagement(ScreenManager):
+	pass
+
+present=Builder.load_file("whatsapp.kv")
+
+
+class WhatsAppApp(App):
+	def build(self):
+		return present
+
+if __name__ == '__main__':
+	WhatsAppApp().run()
